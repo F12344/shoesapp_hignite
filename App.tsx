d1b7749fc31dde47a7f9f41b14e7c6b7f55da61b
unknown <fernando.hms@outlook.com>
2023-05-24 17:30:57 -0300

    Adicionar meu projeto

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { StatusBar } from 'react-native';
+import OneSignal from 'react-native-onesignal';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -8,6 +9,7 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+OneSignal.setAppId('d69f576f-eaf4-4904-a4e5-c6f670b455ef');
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
